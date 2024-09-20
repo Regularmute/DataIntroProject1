0e0bea68aab477f62fefa950ca776fa526efdebf
miahro <mikko.ahro@gmail.com>
2024-09-20 15:33:42 +0300

    fix logics of fmi_fix

--- src/check_fmi.py
+++ src/check_fmi.py
@@ -10,14 +10,13 @@ def list_and_remove_duplicate_rows(file_path, output_file_path):
     if not duplicates.empty:
         print("Duplicate rows based on 'year', 'month', 'day', and 'hour':")
         print(duplicates)
+        df_cleaned = df.drop_duplicates(
+            subset=['year', 'month', 'day', 'hour'], keep='first')
+
+        df_cleaned.to_csv(output_file_path, index=False)
+        print(f"Cleaned data saved to {output_file_path}")
     else:
         print("No duplicate rows found.")
-
-    df_cleaned = df.drop_duplicates(
-        subset=['year', 'month', 'day', 'hour'], keep='first')
-
-    df_cleaned.to_csv(output_file_path, index=False)
-    print(f"Cleaned data saved to {output_file_path}")
 
 
 if __name__ == "__main__":
