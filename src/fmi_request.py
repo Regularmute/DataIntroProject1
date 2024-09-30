91dd3075adfeb661050e04ec8f5b6a2262f00f1b
miahro <mikko.ahro@gmail.com>
2024-09-30 21:38:33 +0300

    improve fmi_request, add k-fold to datafunctions

--- src/fmi_request.py
+++ src/fmi_request.py
@@ -2,7 +2,8 @@
 
 import datetime as dt
 import pandas as pd
-from fmi_config import bbox, stations, timestep, annual_data, annual_data, forecast_places
+
+from fmi_config import bbox, stations, timestep, annual_data, annual_data
 
 from fmiopendata.wfs import download_stored_query
 
@@ -53,7 +54,7 @@ def split_time_intervals(start_time, end_time, interval_hours=72):
         current_end = min(
             current_start + dt.timedelta(hours=interval_hours), end_time)
         intervals.append((current_start, current_end))
-        current_start = current_end
+        current_start = current_end + dt.timedelta(hours=1)
     return intervals
 
 
@@ -84,14 +85,54 @@ def load_dataframe_from_csv(filename):
 
 def load_test():
     """driver or tester for loading history data"""
-    start_time = dt.datetime(2023, 11, 1, 0, 0, 0)
-    end_time = dt.datetime(2023, 12, 31, 23, 0, 0)
+    start_time = dt.datetime(2023, 1, 1, 0, 0, 0)
+    end_time = dt.datetime(2023, 1, 9, 23, 0, 0)
 
     temperature_history = collect_yearly_data(
         start_time, end_time, timestep, stations)
     save_dataframe_to_csv(temperature_history, annual_data)
     loaded_data = load_dataframe_from_csv(annual_data)
     print(loaded_data)
+
+
+def load_pieces(fmi_pieces):
+    """Load historical weather data as monthly pieces"""
+    for start, end, filename in fmi_pieces:
+
+        data = pd.DataFrame()
+        for start_time, end_time, filename in fmi_pieces:
+            temperature_history = collect_yearly_data(
+                start_time, end_time, timestep, stations)
+            save_dataframe_to_csv(temperature_history, filename)
+    # return data
+
+
+def generate_fmi_pieces(start_year=2024, start_month=3, end_year=2024, end_month=9, start_counter=27):
+    fmi_pieces = []
+    months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+              'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+    def get_last_day_of_month(year, month):
+        if month == 12:
+            return dt.datetime(year, 12, 31, 23, 0, 0)
+        next_month = dt.datetime(year, month + 1, 1, 0, 0, 0)
+        last_day = next_month - dt.timedelta(days=1)
+        return dt.datetime(last_day.year, last_day.month, last_day.day, 23, 0, 0)
+
+    counter = start_counter
+    for year in range(start_year, end_year + 1):
+        for month in range(1, 13):
+            if year == start_year and month < start_month:
+                continue
+            if year == end_year and month > end_month:
+                break
+            start_date = dt.datetime(year, month, 1, 0, 0, 0)
+            end_date = get_last_day_of_month(year, month)
+            file_name = f'data/fmi_pieces/{counter:02d}_{months[month-1]}_{year}.csv'
+            fmi_pieces.append((start_date, end_date, file_name))
+            counter += 1
+
+    return fmi_pieces
 
 
 def fmi_forecast_query(place):
@@ -127,11 +168,16 @@ def forecast_query(places):
 
 
 if __name__ == "__main__":
+    time_splits = generate_fmi_pieces(
+        start_year=2022, start_month=6, end_year=2024, end_month=9, start_counter=6)
+    # print(time_splits)
 
-    df = forecast_query(forecast_places)
-    print(df)
-    fc = fmi_forecast_query("helsinki")
+    df = load_pieces(time_splits)
 
-    load_test()
+    # df = forecast_query(forecast_places)
+    # print(df)
+    # fc = fmi_forecast_query("helsinki")
+
+    # load_test()
 
 #    print(fc.data.keys())
