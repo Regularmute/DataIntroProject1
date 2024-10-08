285a43549a57bca903fb4963957c3eca9828675b
Regularmute <93884347+Regularmute@users.noreply.github.com>
2024-10-08 12:17:00 +0300

    feature: add function to return fingrid df

--- src/request_builder.py
+++ src/request_builder.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json
 from dotenv import load_dotenv
 from io import StringIO
+from time import sleep
 
 load_dotenv()
 headers = {'Cache-Control':'no-cache', 'x-api-key': os.getenv('FINGRID_API_KEY')}
@@ -12,6 +13,20 @@ def get_fingrid_url(datasetId, s_year, s_month, s_day, s_hour, s_min, e_year, e_
     startTime = f"{s_year}-{s_month:02d}-{s_day:02d}T{s_hour:02d}:{s_min:02d}:00Z"
     endTime = f"{e_year}-{e_month:02d}-{e_day:02d}T{e_hour:02d}:{e_min:02d}:00Z"
     return f'https://data.fingrid.fi/api/datasets/{datasetId}/data?startTime={startTime}&endTime={endTime}&format=json&pageSize=100'
+
+def get_dataframe_by_url(url):
+    sleep(3)
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        df = pd.read_json(StringIO(json.dumps(r.json()['data'])))
+        time = df.startTime.str.split(r'[:\-\.TZ]', expand=True).iloc[:,0:5]
+        time.columns = ['year', 'month', 'day', 'hour', 'min']
+        df = pd.concat([time, df.value], axis=1)
+        return(df)
+    else:
+        print("Error", r.status_code)
+        result_json = r.json()
+        print(result_json)
 
 def load_fingrid_data():
     pass
